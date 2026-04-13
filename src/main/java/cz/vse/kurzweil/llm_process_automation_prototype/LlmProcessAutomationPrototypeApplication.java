bf877d210fe8785330cf418b63a9149b83135996
Jan Kurzweil <Jan-Kurzweil@seznam.cz>
2026-04-13 20:18:38 +0200

    Initial commit

---
+++ src/main/java/cz/vse/kurzweil/llm_process_automation_prototype/LlmProcessAutomationPrototypeApplication.java
@@ -0,0 +1,13 @@
+package cz.vse.kurzweil.llm_process_automation_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmProcessAutomationPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmProcessAutomationPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/vse/kurzweil/llm_process_automation_prototype/LlmProcessAutomationPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vse.kurzweil.llm_process_automation_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmProcessAutomationPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
