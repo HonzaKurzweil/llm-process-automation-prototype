eab729bf6cda64946e1e38bfeede008ea45937a5
Jan Kurzweil <Jan-Kurzweil@seznam.cz>
2026-04-18 12:10:42 +0200

    ServiceImpl used

--- src/main/java/cz/vse/kurzweil/llm_process_automation_prototype/service/StructuredOutputService.java
+++ src/main/java/cz/vse/kurzweil/llm_process_automation_prototype/service/StructuredOutputService.java
@@ -1,37 +1,8 @@
 package cz.vse.kurzweil.llm_process_automation_prototype.service;
 
 import cz.vse.kurzweil.llm_process_automation_prototype.dto.newmobileorder.NewMobileOrderRequest;
-import cz.vse.kurzweil.llm_process_automation_prototype.service.extraction.PromptStrategy;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+public interface StructuredOutputService {
 
-@Slf4j
-@Service
-public class StructuredOutputService {
-
-    private final Map<ExtractionMode, PromptStrategy> promptStrategies;
-    private final Map<ModelType, ChatClient> modelClients;
-
-    public StructuredOutputService(
-            List<PromptStrategy> strategies,
-            @Qualifier("gpt4oMini") ChatClient gpt4oMiniClient,
-            @Qualifier("gpt4o") ChatClient gpt4oClient) {
-        this.promptStrategies = strategies.stream()
-                .collect(Collectors.toMap(PromptStrategy::mode, s -> s));
-        this.modelClients = Map.of(
-                ModelType.GPT_4O_MINI, gpt4oMiniClient,
-                ModelType.GPT_4O, gpt4oClient
-        );
-    }
-
-    public NewMobileOrderRequest extract(String inputText, ExtractionMode mode, ModelType model) {
-        log.info("Extracting with mode={}, model={}", mode, model);
-        return promptStrategies.get(mode).extract(inputText, modelClients.get(model));
-    }
+    NewMobileOrderRequest extract(String inputText, ExtractionMode mode, ModelType model);
 }
