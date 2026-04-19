822bf531dafd5788084edc65e0867e90ccb3d285
Jan Kurzweil <Jan-Kurzweil@seznam.cz>
2026-04-19 19:15:37 +0200

    execution service init

---
+++ src/main/java/cz/vse/kurzweil/llm_process_automation_prototype/service/execution/ExecutionService.java
@@ -0,0 +1,4 @@
+package cz.vse.kurzweil.llm_process_automation_prototype.service.execution;
+
+public interface ExecutionService {
+}
---
+++ src/main/java/cz/vse/kurzweil/llm_process_automation_prototype/service/execution/ExecutionServiceImpl.java
@@ -0,0 +1,11 @@
+package cz.vse.kurzweil.llm_process_automation_prototype.service.execution;
+
+import java.nio.file.Path;
+
+public class ExecutionServiceImpl implements ExecutionService{
+
+    public void execute(Path inputFile) {
+
+        //  parse input file -> RequestType
+    }
+}
