b5ebfe7acdd2c814c9b3cd5987eade1252f223b8
Jan Kurzweil <Jan-Kurzweil@seznam.cz>
2026-04-25 10:41:50 +0200

    new models added

--- src/main/java/cz/vse/kurzweil/llm_process_automation_prototype/dto/ModelType.java
+++ src/main/java/cz/vse/kurzweil/llm_process_automation_prototype/dto/ModelType.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ModelType {
+    GPT_5_MINI("gpt-5-mini"),
+    GPT_5_NANO("gpt-5-nano"),
     GPT_4O_MINI("gpt-4o-mini"),
     GPT_4O("gpt-4o");
 
